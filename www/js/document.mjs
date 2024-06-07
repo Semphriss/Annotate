Semphriss/Annotate

commit 0bad9c4f7c8ed7c34ec66741972088989af3af9c

Cache Base64 copy of PDF for performance

www/js/document.mjs
@@ -17,21 +17,8 @@ import { DocumentPage } from './document_page.mjs';
 import { Pdf } from './pdf.mjs';
 import { Job } from './job.mjs';
 import { saveFile } from './file_manager.mjs';
+import { Uint8ArrayToBase64 } from './b64.mjs';
 import { PDFDocument } from '../pdf-lib.esm.min.js';
-
-function Uint8ArrayToBase64(arr) {
-  var binary = '';
-
-  for (const byte of arr) {
-    binary += String.fromCharCode(byte);
-  }
-
-  return window.btoa(binary);
-}
-
-function Base64ToUint8Array(b64) {
-  return Uint8Array.from(atob(b64), c => c.charCodeAt(0));
-}
 
 /**
  * A document, as managed by Annotate.
@@ -55,7 +42,7 @@ export class Document {
     const pdfData = dataPages.shift();
 
     if (pdfData.length !== 0) {
-      that.pdf = await Pdf.fromData(Base64ToUint8Array(pdfData));
+      that.pdf = await Pdf.fromData(pdfData);
     }
 
     that.container = container;
@@ -141,7 +128,7 @@ export class Document {
     let serialized = '';
 
     if (this.pdf) {
-      serialized += Uint8ArrayToBase64(await this.pdf.getData());
+      serialized += this.pdf.getB64Data();
     }
 
     for (const page of this.pages) {

www/js/pdf.mjs
@@ -15,6 +15,7 @@
 
 import { pdfjsLib } from './pdfjs.mjs';
 import { PdfPage } from './pdf_page.mjs';
+import { Base64ToUint8Array } from './b64.mjs';
 
 /**
  * A PDF document.
@@ -22,11 +23,15 @@ import { PdfPage } from './pdf_page.mjs';
 export class Pdf {
   /* pdfjs document handle */ doc;
   /* PdfDocumentPage[] */ pages = [];
+  /* string */ cachedB64Data = null;
 
-  static async fromData(data) {
+  static async fromData(b64data) {
+    const data = Base64ToUint8Array(b64data);
+
     const that = new Pdf();
 
     that.doc = await pdfjsLib.getDocument({ data: data }).promise;
+    that.cachedB64Data = b64data;
 
     return that;
   }
@@ -47,6 +52,10 @@ export class Pdf {
     }
 
     return this.pages[num];
+  }
+
+  getB64Data() {
+    return this.cachedB64Data;
   }
 
   async getData() {

www/js/b64.mjs
@@ -1,0 +1,28 @@
+//  Annotate - View and annotate your PDF files
+//  Copyright 2022-2024 Semphris <semphris@semphris.com>
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+export function Uint8ArrayToBase64(arr) {
+  var binary = '';
+
+  for (const byte of arr) {
+    binary += String.fromCharCode(byte);
+  }
+
+  return window.btoa(binary);
+}
+
+export function Base64ToUint8Array(b64) {
+  return Uint8Array.from(atob(b64), c => c.charCodeAt(0));
+}

commit bc9179a963d85a780bfdb2c864700c8c84fd95f3

Fix file import
Commit 0bad9c4 broke file import; this fixes it.

www/js/document.mjs
@@ -42,7 +42,7 @@ export class Document {
     const pdfData = dataPages.shift();
 
     if (pdfData.length !== 0) {
-      that.pdf = await Pdf.fromData(pdfData);
+      that.pdf = await Pdf.fromBase64(pdfData);
     }
 
     that.container = container;
@@ -68,7 +68,7 @@ export class Document {
   static async fromPdfData(data, container, name) {
     const that = new Document();
 
-    that.pdf = await Pdf.fromData(data);
+    that.pdf = await Pdf.fromDataString(data);
     that.container = container;
     that.name = name;
 

www/js/pdf.mjs
@@ -15,7 +15,7 @@
 
 import { pdfjsLib } from './pdfjs.mjs';
 import { PdfPage } from './pdf_page.mjs';
-import { Base64ToUint8Array } from './b64.mjs';
+import { Uint8ArrayToBase64, Base64ToUint8Array } from './b64.mjs';
 
 /**
  * A PDF document.
@@ -25,9 +25,19 @@ export class Pdf {
   /* PdfDocumentPage[] */ pages = [];
   /* string */ cachedB64Data = null;
 
-  static async fromData(b64data) {
-    const data = Base64ToUint8Array(b64data);
+  static async fromDataString(data) {
+    return await Pdf.fromDataAndBase64(data, btoa(data));
+  }
 
+  static async fromDataUint8Array(data) {
+    return await Pdf.fromDataAndBase64(data, Uint8ArrayToBase64(data));
+  }
+
+  static async fromBase64(b64data) {
+    return await Pdf.fromDataAndBase64(Base64ToUint8Array(b64data), b64data);
+  }
+
+  static async fromDataAndBase64(data, b64data) {
     const that = new Pdf();
 
     that.doc = await pdfjsLib.getDocument({ data: data }).promise;